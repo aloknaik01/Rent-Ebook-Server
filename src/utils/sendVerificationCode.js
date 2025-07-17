de2353f382791fe01f0df81d6e0e06a40b75445e
aloknaik <aloknaik016261@gmail.com>
2025-07-17 05:42:34 +0530

    feat(auth): send verification code via email with success and error responses

---
+++ src/utils/sendVerificationCode.js
@@ -0,0 +1,21 @@
+export async function sendVerificationCode(verificationCode, email, res) {
+  try {
+    const vCode = generateVOtpTemplate(verificationCode);
+    snedEmail({
+      email,
+      subject: 'verification code',
+      messgage,
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "verification code send successfully!".
+    });
+
+  } catch (error) {
+    res.status(500).jon({
+      success: false,
+      messgage: 'Failed to send verification code!',
+    });
+  }
+}
